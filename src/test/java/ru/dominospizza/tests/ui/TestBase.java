ff370af834820bdf5f906046eb85fe214988c246
Spitsyn Kirill <kaspicin2602@gmail.com>
2023-02-22 21:04:13 +0300

    ui attach

--- src/test/java/ru/dominospizza/tests/ui/TestBase.java
+++ src/test/java/ru/dominospizza/tests/ui/TestBase.java
@@ -1,7 +1,8 @@
 package ru.dominospizza.tests.ui;
 
 import com.codeborne.selenide.Configuration;
-import com.sun.tools.javac.Main;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.dominospizza.tests.ui.config.Config;
+import ru.dominospizza.tests.ui.helpers.Attach;
 import ru.dominospizza.tests.ui.pages.MainPage;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
@@ -40,7 +42,7 @@ public class TestBase {
 
 
                 DesiredCapabilities capabilities = new DesiredCapabilities();
-                capabilities.setCapability(CapabilityType.PAGE_LOAD_STRATEGY,"eager");
+                capabilities.setCapability(CapabilityType.PAGE_LOAD_STRATEGY, "eager");
                 capabilities.setCapability("enableVNC", true);
                 capabilities.setCapability("enableVideo", true);
                 Configuration.browserCapabilities = capabilities;
@@ -49,12 +51,18 @@ public class TestBase {
 
     @BeforeEach
     void beforeEach() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         mainPage.openPage();
     }
 
     @AfterEach
     @DisplayName("Закрыть браузер")
     void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
 }
+

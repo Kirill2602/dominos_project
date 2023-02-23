6f61a7ecbc869f16c91454a5d19264b38837cbab
Spitsyn Kirill <kaspicin2602@gmail.com>
2023-02-23 19:14:26 +0300

    ui

--- src/test/java/ru/dominospizza/tests/ui/tests/UITests.java
+++ src/test/java/ru/dominospizza/tests/ui/tests/UITests.java
@@ -23,138 +23,138 @@ public class UITests extends TestBase {
     TestData data = new TestData();
     MainPage mainPage = new MainPage();
 
-//    @Test
-//    @DisplayName("Проверка всех пунктов меню в шапке главной страницы")
-//    @Tag("ui")
-//    @Owner("spitsyn.k")
-//    @Severity(TRIVIAL)
-//    void checkMainPageHeaderLogoAndNavMenuItems() {
-//        mainPage
-//                .checkVisibilityOfHeaderLogo()
-//                .checkNavMenuItems(data.getNavMenuItemsText());
-//    }
-//
-//    @Test
-//    @DisplayName("Неудачная попытка авторизации с пустыми полями 'Номер телефона' и 'Пароль'")
-//    @Tag("ui")
-//    @Owner("spitsyn.k")
-//    @Severity(NORMAL)
-//    void unSuccessLoginWithEmptyPhoneNumberAndPassword() {
-//        mainPage
-//                .clickLoginButton()
-//                .checkingForTabsOfForm()
-//                .chooseAnAuthorizationMethod()
-//                .incorrectLogin("", "")
-//                .submitLoginButton()
-//                .checkPhoneErrorText(data.getExpectedEmptyPhoneErrorText())
-//                .checkPasswordErrorText(data.getExpectedEmptyPasswordErrorText());
-//    }
-//
-//    @Test
-//    @DisplayName("Неудачная попытка авторизации с пустым номером телефона")
-//    @Tag("ui")
-//    @Owner("spitsyn.k")
-//    @Severity(NORMAL)
-//    void unSuccessLoginWithEmptyPhoneNumber() {
-//        mainPage
-//                .clickLoginButton()
-//                .checkingForTabsOfForm()
-//                .chooseAnAuthorizationMethod()
-//                .incorrectLogin("", "qwe123456")
-//                .submitLoginButton()
-//                .checkPhoneErrorText(data.getExpectedEmptyPhoneErrorText());
-//    }
-//
-//    @Test
-//    @DisplayName("Неудачная попытка авторизации с пустым паролем")
-//    @Tag("ui")
-//    @Owner("spitsyn.k")
-//    @Severity(NORMAL)
-//    void unSuccessLoginWithEmptyPassword() {
-//        mainPage
-//                .clickLoginButton()
-//                .checkingForTabsOfForm()
-//                .chooseAnAuthorizationMethod()
-//                .incorrectLogin("89677777277", "")
-//                .submitLoginButton()
-//                .checkPasswordErrorText(data.getExpectedEmptyPasswordErrorText());
-//    }
-//
-//    @Test
-//    @DisplayName("Неудачная попытка авторизации с коротким номером телефона")
-//    @Tag("ui")
-//    @Owner("spitsyn.k")
-//    @Severity(NORMAL)
-//    void unSuccessLoginWithShortPhoneNumber() {
-//        mainPage
-//                .clickLoginButton()
-//                .checkingForTabsOfForm()
-//                .chooseAnAuthorizationMethod()
-//                .incorrectLogin("11111111", "qwe123456")
-//                .submitLoginButton()
-//                .checkPhoneErrorText(data.getExpectedShortPhoneErrorText());
-//    }
-//
-//    @Test
-//    @DisplayName("Успешная авторизация зарегестрированного пользователя")
-//    @Tag("ui")
-//    @Owner("spitsyn.k")
-//    @Severity(BLOCKER)
-//    void successLogin() {
-//        mainPage
-//                .clickLoginButton()
-//                .checkingForTabsOfForm()
-//                .chooseAnAuthorizationMethod()
-//                .correctLogin("9652294486", "Qwe12345")
-//                .checkUserName(data.getExpectedUserName());
-//    }
-//
-//    @Test
-//    @DisplayName("Добавление товара в корзину")
-//    @Tag("ui")
-//    @Owner("spitsyn.k")
-//    @Severity(BLOCKER)
-//    void addProductInCart() {
-//        mainPage
-//                .clickOnPizzaLink()
-//                .checkAvailabilityOfPizzaName("Пицца-туница")
-//                .clickChoiceButton()
-//                .clickAddButton()
-//                .checkQtyInCart()
-//                .clickCartButton()
-//                .checkPizzaName("Пицца-туница")
-//                .checkQtyInCart("1")
-//                .clickArrangeOrderButton()
-//                .checkHeaderText("Моя корзина")
-//                .checkProductNameInDescription("Пицца-туница")
-//                .checkQty("1")
-//                .checkVisibilityOfRegisterOrderButton();
-//    }
-//
-//    @Test
-//    @DisplayName("Редактирование кол-ва товара в корзине, проверка цены и удаление товара")
-//    @Tag("ui")
-//    @Owner("spitsyn.k")
-//    @Severity(BLOCKER)
-//    void changeProductQtyAndCheckPriceInCartAndDeleteProduct() {
-//        mainPage
-//                .clickOnPizzaLink()
-//                .checkAvailabilityOfPizzaName("Пицца-туница")
-//                .clickChoiceButton()
-//                .clickAddButton()
-//                .checkQtyInCart()
-//                .clickCartButton()
-//                .clickArrangeOrderButton()
-//                .checkHeaderText("Моя корзина")
-//                .checkProductNameInDescription("Пицца-туница")
-//                .checkQty("1")
-//                .checkQtyAndPriceAfterClickOnPlusBtn()
-//                .checkQty("2")
-//                .deleteProduct()
-//                .checkVisibilityOfHeaderText()
-//                .clickYesButton()
-//                .checkVisibilityOfEmptyCartText();
-//    }
+    @Test
+    @DisplayName("Проверка всех пунктов меню в шапке главной страницы")
+    @Tag("ui")
+    @Owner("spitsyn.k")
+    @Severity(TRIVIAL)
+    void checkMainPageHeaderLogoAndNavMenuItems() {
+        mainPage
+                .checkVisibilityOfHeaderLogo()
+                .checkNavMenuItems(data.getNavMenuItemsText());
+    }
+
+    @Test
+    @DisplayName("Неудачная попытка авторизации с пустыми полями 'Номер телефона' и 'Пароль'")
+    @Tag("ui")
+    @Owner("spitsyn.k")
+    @Severity(NORMAL)
+    void unSuccessLoginWithEmptyPhoneNumberAndPassword() {
+        mainPage
+                .clickLoginButton()
+                .checkingForTabsOfForm()
+                .chooseAnAuthorizationMethod()
+                .incorrectLogin("", "")
+                .submitLoginButton()
+                .checkPhoneErrorText(data.getExpectedEmptyPhoneErrorText())
+                .checkPasswordErrorText(data.getExpectedEmptyPasswordErrorText());
+    }
+
+    @Test
+    @DisplayName("Неудачная попытка авторизации с пустым номером телефона")
+    @Tag("ui")
+    @Owner("spitsyn.k")
+    @Severity(NORMAL)
+    void unSuccessLoginWithEmptyPhoneNumber() {
+        mainPage
+                .clickLoginButton()
+                .checkingForTabsOfForm()
+                .chooseAnAuthorizationMethod()
+                .incorrectLogin("", "qwe123456")
+                .submitLoginButton()
+                .checkPhoneErrorText(data.getExpectedEmptyPhoneErrorText());
+    }
+
+    @Test
+    @DisplayName("Неудачная попытка авторизации с пустым паролем")
+    @Tag("ui")
+    @Owner("spitsyn.k")
+    @Severity(NORMAL)
+    void unSuccessLoginWithEmptyPassword() {
+        mainPage
+                .clickLoginButton()
+                .checkingForTabsOfForm()
+                .chooseAnAuthorizationMethod()
+                .incorrectLogin("89677777277", "")
+                .submitLoginButton()
+                .checkPasswordErrorText(data.getExpectedEmptyPasswordErrorText());
+    }
+
+    @Test
+    @DisplayName("Неудачная попытка авторизации с коротким номером телефона")
+    @Tag("ui")
+    @Owner("spitsyn.k")
+    @Severity(NORMAL)
+    void unSuccessLoginWithShortPhoneNumber() {
+        mainPage
+                .clickLoginButton()
+                .checkingForTabsOfForm()
+                .chooseAnAuthorizationMethod()
+                .incorrectLogin("11111111", "qwe123456")
+                .submitLoginButton()
+                .checkPhoneErrorText(data.getExpectedShortPhoneErrorText());
+    }
+
+    @Test
+    @DisplayName("Успешная авторизация зарегестрированного пользователя")
+    @Tag("ui")
+    @Owner("spitsyn.k")
+    @Severity(BLOCKER)
+    void successLogin() {
+        mainPage
+                .clickLoginButton()
+                .checkingForTabsOfForm()
+                .chooseAnAuthorizationMethod()
+                .correctLogin("9652294486", "Qwe12345")
+                .checkUserName(data.getExpectedUserName());
+    }
+
+    @Test
+    @DisplayName("Добавление товара в корзину")
+    @Tag("ui")
+    @Owner("spitsyn.k")
+    @Severity(BLOCKER)
+    void addProductInCart() {
+        mainPage
+                .clickOnPizzaLink()
+                .checkAvailabilityOfPizzaName("Пицца-туница")
+                .clickChoiceButton()
+                .clickAddButton()
+                .checkQtyInCart()
+                .clickCartButton()
+                .checkPizzaName("Пицца-туница")
+                .checkQtyInCart("1")
+                .clickArrangeOrderButton()
+                .checkHeaderText("Моя корзина")
+                .checkProductNameInDescription("Пицца-туница")
+                .checkQty("1")
+                .checkVisibilityOfRegisterOrderButton();
+    }
+
+    @Test
+    @DisplayName("Редактирование кол-ва товара в корзине, проверка цены и удаление товара")
+    @Tag("ui")
+    @Owner("spitsyn.k")
+    @Severity(BLOCKER)
+    void changeProductQtyAndCheckPriceInCartAndDeleteProduct() {
+        mainPage
+                .clickOnPizzaLink()
+                .checkAvailabilityOfPizzaName("Пицца-туница")
+                .clickChoiceButton()
+                .clickAddButton()
+                .checkQtyInCart()
+                .clickCartButton()
+                .clickArrangeOrderButton()
+                .checkHeaderText("Моя корзина")
+                .checkProductNameInDescription("Пицца-туница")
+                .checkQty("1")
+                .checkQtyAndPriceAfterClickOnPlusBtn()
+                .checkQty("2")
+                .deleteProduct()
+                .checkVisibilityOfHeaderText()
+                .clickYesButton()
+                .checkVisibilityOfEmptyCartText();
+    }
 
     static Stream<Arguments> checkProducts() {
         return Stream.of(
